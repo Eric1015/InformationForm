7632cd7e1db8986a8aeaeb58fabbe127a4275f0f
love1 <love1dbsb77@hotmail.com>
2018-06-09 00:25:33 -0700

    Set the column of User class

--- db/migrate/20180609072028_create_users.rb
+++ db/migrate/20180609072028_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.email_field :email
+      t.string :address
+      t.text :message
       t.timestamps
     end
   end
