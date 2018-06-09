a36f49cf8da152f36ee42b02943abe888402a4cb
love1 <love1dbsb77@hotmail.com>
2018-06-09 00:48:57 -0700

    Navigate the top page to /signup and invoke the new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/signup' => 'users#index'
+  get '/signup' => 'users#new'
+  resources :users
 end
